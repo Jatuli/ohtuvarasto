9da89d4f113bbf959109924a4930862c7944d0a2
Mauno Pasanen <mepasane@gmail.com>
2025-11-03 20:11:01 +0200

    Testing coverage 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,14 +10,48 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_konstruktori_negatiivinen_tilavuus(self):
+        varasto = Varasto(10, alku_saldo=-5)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_konstruktori_nolla_tilavuus(self):
+        varasto = Varasto(0)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_konstruktori_liikaa_saldoa(self):
+        varasto = Varasto(10, alku_saldo=12)
+        self.assertAlmostEqual(varasto.tilavuus, 10)
+
+    
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
+    def test_lisaa_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
 
+    def test_lisaa_saldoa_liikaa(self):
+        self.varasto.lisaa_varastoon(20)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_ota_varastosta_negatiivinen(self):
+        self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_ota_varastosta_liikaa(self):
+        self.varasto.ota_varastosta(12)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    
+    
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -36,5 +70,7 @@ class TestVarasto(unittest.TestCase):
 
         self.varasto.ota_varastosta(2)
 
+        odotettu = "saldo = 6, vielä tilaa 4"
+
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
-        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        self.assertEqual(str(self.varasto), odotettu)
