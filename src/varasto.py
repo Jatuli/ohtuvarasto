1ed3387d5b57def2fabf0a270c565bcfcfe11de5
Mauno Pasanen <mepasane@gmail.com>
2025-11-10 20:09:04 +0200

    bug fix to varasto.py

--- src/varasto.py
+++ src/varasto.py
@@ -29,4 +29,4 @@ class Varasto:
         return maara
 
     def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+        return f"saldo = {int(self.saldo)}, vielä tilaa {int(self.paljonko_mahtuu())}"
